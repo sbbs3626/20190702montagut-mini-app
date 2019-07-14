ed8e935259aae2927bce52746a44686ca9b2976c
sbbs3626 <362670070@qq.com>
2019-07-14 22:48:10 +0800

    问卷列表

--- pages/question/question.js
+++ pages/question/question.js
@@ -1,19 +1,22 @@
 // pages/question/question.js
-import {question} from '../../utils/util.js';
+import { question } from '../../utils/util.js';
+const _ = require('lodash');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    questionList:[],
+    isSelect: [{ id: 0 }, { id: 0 }, { id: 0 }, { id: 0 }, { id: 0 }, { id: 0 }],
+    a:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(question)
+    this._question();
   },
 
   /**
@@ -22,46 +25,26 @@ Page({
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  _question(){
+    let arr = _.sampleSize(question, 6);
+    // 随机6个题目
+    this.setData({
+      questionList: arr
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  _next:function(e){
+    console.log(e)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  _select(e){
+    let isSelect = this.data.isSelect;
+    isSelect[e.currentTarget.dataset.index]={
+      'id': e.currentTarget.id,
+      'val': e.currentTarget.dataset.val
+    }
+    this.setData({
+      isSelect: isSelect
+    })
+    console.log(this.data.isSelect)
   }
+
 })
